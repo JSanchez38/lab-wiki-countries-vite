287545f015b69178c442aeb874a19c86159af53a
Jesús Sánchez <jsxexu38@gmail.com>
2024-04-13 15:04:03 +0200

    feat: iteration 1,2,3,4

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,16 @@
-function CountryDetails() {}
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import countryData from '../data/countries.json'
+
+
+function CountryDetails() {
+    const [countryDetails, setCountryDetails] = useState(countryData)
+
+    const { countryId } = useParams()
+
+    return (
+        <h1>Hola</h1>
+    )
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,22 @@
-function HomePage() {}
+import { useState, useEffect } from "react";
+import countriesData from '../data/countries.json'
+import { Link } from 'react-router-dom'
+
+function HomePage() {
+    const [countries, setCountries] = useState(countriesData)
+    return (
+        <>
+        <h1>WikiCountries: Your Guide to the World</h1>
+        {countries.map(country => (
+            <div key={country._id}>
+                <Link to={`/${country.alpha3Code}`}>
+                    <p>{country.name.common}</p>
+                    <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} />
+                </Link>
+            </div>
+        ))}
+        </>
+    )
+}
 
 export default HomePage;
